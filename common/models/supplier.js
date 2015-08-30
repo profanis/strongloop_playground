9df641f34af9ddb2b463090128e7376e62332d9e
profanis <fanis@projectbeagle.com>
2015-08-30 17:00:36 +0300

    new playgroung

---
+++ common/models/supplier.js
@@ -0,0 +1,30 @@
+module.exports = function (Supplier) {
+	
+	Supplier.beforeRemote('create', function(context, user, next) {
+	    var req = context.req;
+	    req.body.date = Date.now();
+	    req.body.publisherId = req.accessToken.userId;
+	    next();
+	  });
+	
+	var isStatic = true;
+	Supplier.disableRemoteMethod('find', isStatic);
+	Supplier.disableRemoteMethod('findById', isStatic);
+		
+	Supplier.status = function (cb) {
+
+		var status = Supplier.create({
+			"name": "Fanissss",
+			"address": "test address",
+			"email": "fanis@test.com"
+		});
+
+
+		cb(null, status);
+	};
+	Supplier.remoteMethod(
+		'status', {
+			http: { path: '/status', verb: 'get' },
+			returns: { arg: 'status', type: 'string' }
+		});
+};
---
+++ server/boot/sampleData.js
@@ -0,0 +1,41 @@
+var async = require("async");
+module.exports = function (server) {
+  var mongoDs = server.dataSources.mongo;
+
+  createUsers();
+
+  async.parallel({
+    suppliers: async.apply(createSuppliers)
+  }, function (err, results) {
+      if (err) throw err;
+      console.log("Sample Suppliers succesfully created");
+    });
+
+  function createSuppliers(cb) {
+
+    mongoDs.automigrate("supplier", function (err) {
+      if (err) return cb(err);
+      var Supplier = server.models.supplier;
+
+      Supplier.create([
+        { "name": "Fanis", "address": "test Fani's address", "email": "fanis@test.com" },
+        { "name": "Tania", "address": "test Tania's", "email": "tania@test.com" },
+        { "name": "Sasaxnh", "address": "test Sasaxnh's address", "email": "sasaxnh@test.com" }
+      ]);
+    });
+  }
+
+  function createUsers() {
+    var User = server.models.User;
+
+    User.create({ email: "prodromouf@gmail.com", password: "qqq111" }, function (err, user) {
+      if (err) throw err;
+
+      User.login({ email: 'prodromouf@gmail.com', password: 'qqq111' }, function (err, accessToken) {
+        if(err) console.log(err);
+        
+        console.log(accessToken);
+      });
+    });
+  }
+};
